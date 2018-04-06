370c998cc52a4f4d40743175896231c5818768cb
jiaojq <jiaojq666@163.com>
2018-04-06 11:26:13 +0800

    绑定手机号

---
+++ src/components/base/inputer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrap">
+    <div class="left" v-if="left">{{left}}</div>
+    <div class="center"><input type="text"></div>
+    <div class="right" v-if="right">{{right}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['left', 'right']
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrap
+  background #fff
+  width 100%
+  display flex
+  .left
+    width 20%
+  .right
+    width 30%
+  .center 
+    flex 1
+</style>
--- src/pages/bind-phone/index.vue
+++ src/pages/bind-phone/index.vue
@@ -1,15 +1,88 @@
 <template>
   <div class="container">
-
+    <button class="button" size="default">一键绑定手机号</button>
+    <div class="bind-title">手动绑定手机号</div>
+    <div class="user-from">
+      <div class="text">手机号</div>
+      <div class="input">
+        <input type="text" placeholder="输入手机号" placeholder-style='font-size: 15px'>
+      </div>
+    </div>
+    <div class="user-from sec">
+      <div class="input">
+        <input type="text" placeholder="输入手机号" placeholder-style='font-size: 15px'>
+      </div>
+      <div class="text">
+        <div class="buttons">
+          获取验证码
+        </div>
+      </div>
+    </div>
+    <div class="button-radio"></div>
+    <div class="tiaokuan">
+      我已阅读并同意《小确幸商户服务条款）
+    </div>
+    <button class="button sub" size="default">确定</button>
   </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+.container
+  .button
+    &.sub
+      margin-top 10px
+    width 80%
+    background #616161
+    color #fff
+    margin-top 75px
+  .bind-title
+    line-height 45px
+    text-align center 
+    font-size 14px
+    margin-top 100px
+  .user-from
+    background #fff
+    display flex
+    line-height 50px
+    height 50px
+    border-bottom 1px solid #ccc
+    align-items center
+    &.sec
+      border-color #F0F0F2
+    .text
+      width 25%
+      font-size 13px
+      text-align center 
+    .input 
+      flex 1
+      font-size 15px
+      padding-left 25px
+    .buttons 
+      width 90px
+      height 30px
+      background #616161
+      line-height 30px
+      text-align center 
+      color #fff
+      border-radius 4px
+      position relative
+      right 22px
+  .button-radio
+    width 15px
+    height 15px
+    border 1px solid #101010
+    border-radius 20px
+    margin auto
+    margin-top 50px
+  .tiaokuan
+    color #101010
+    font-size 12px
+    text-align center 
+    line-height 17px 
+    margin-top 10px
 </style>
